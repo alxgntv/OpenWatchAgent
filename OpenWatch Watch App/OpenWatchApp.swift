edd553f1510a3f1d8e29eff02d4ac55ae49715c3
alxgntv <support@code.market>
2026-06-03 14:52:15 +0300

    Initial Commit

---
+++ OpenWatch Watch App/OpenWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWatchApp.swift
+//  OpenWatch Watch App
+//
+//  Created by Alex Ign on 03.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenWatch/OpenWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWatchApp.swift
+//  OpenWatch
+//
+//  Created by Alex Ign on 03.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
